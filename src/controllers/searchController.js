9e710804d0fb75be568fc52ab843205e9be38daf
Rpsxxxxxxn <keito0122.y@gmail.com>
2023-10-09 15:29:42 +0900

    検索処理の実装

--- src/controllers/searchController.js
+++ src/controllers/searchController.js
@@ -2,14 +2,26 @@
  * 商品の検索を行う
  */
 
+const DatabaseUtils = require("../db/databaseUtils");
+
 /**
  * 検索処理
  * @param {*} req リクエストパラメータ
  * @param {*} res レスポンスパラメータ
  */
 exports.searchPost = (req, res) => {
-  const keyward = req.query.keyward;
-  console.log(keyward);
+  const keyward = req.body.search;
+  if (keyward === null || keyward === undefined || keyward === '') {
+    res.status(400).send({ error: '文字列で送信してください。' });
+  }
 
-  res.status(200).send({ keyward: keyward });
+  DatabaseUtils.allQuery("SELECT * FROM products WHERE name LIKE ?", [`%${keyward}%`])
+  .then((rows) => {
+    console.log(rows);
+    res.status(200).send({ products: rows });
+  })
+  .catch((err) => {
+    console.log(err);
+    res.status(500).send({ error: err });
+  });
 }
